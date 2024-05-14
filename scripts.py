3bf0623d1b26fdd1de817cc64ef7cdee480fc8cc
DanilaKorsakov <kvashnindan@gmail.com>
2024-05-14 13:34:18 +0300

    created new const variable, using update method, new variables names

--- scripts.py
+++ scripts.py
@@ -4,15 +4,23 @@ from datacenter.models import Chastisement
 from datacenter.models import Lesson
 from datacenter.models import Commendation
 import random
-from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
+
+
+COMPLIMENTS = [
+    'Молодец!',
+    'Отлично!',
+    'Хорошо!',
+    'Гораздо лучше, чем я ожидал!',
+    'Великолепно!',
+    'Прекрасно!',
+    'Ты меня очень обрадовал!',
+]
 
 
 def fix_marks(schoolkid):
     marks=Mark.objects.filter(schoolkid=schoolkid)
     bad_marks=marks.filter(points__in=[2,3])
-    for bad_mark in  bad_marks:
-        bad_mark.points=5
-        bad_mark.save()
+    bad_marks.update(points=5)
 
 
 def delete_chastisements(schoolkid):
@@ -22,18 +30,18 @@ def delete_chastisements(schoolkid):
 
 def create_commendation(schoolkid, subject_name):
     lessons=Lesson.objects.filter(year_of_study=schoolkid.year_of_study, group_letter=schoolkid.group_letter)
-    needed_subject = lessons.filter(subject__title=subject_name).order_by('date').first()
-    texts = ['Молодец!','Отлично!','Хорошо!','Гораздо лучше, чем я ожидал!','Великолепно!','Прекрасно!','Ты меня очень обрадовал!',]
+    lesson = lessons.filter(subject__title=subject_name).order_by('date').first()
 
-    Commendation.objects.create(schoolkid=schoolkid, teacher=needed_subject.teacher,subject=needed_subject.subject,created=needed_subject.date,text=random.choice(texts))
+
+    Commendation.objects.create(schoolkid=schoolkid, teacher=lesson.teacher,subject=lesson.subject,created=lesson.date,text=random.choice(COMPLIMENTS))
 
 
 def get_schoolkid(full_name):
     try:
         return Schoolkid.objects.get(full_name__contains=full_name)
-    except ObjectDoesNotExist:
+    except Schoolkid.ObjectDoesNotExist:
         print("Такого ученика не существует")
-    except MultipleObjectsReturned:
+    except Schoolkid.MultipleObjectsReturned:
         print("Нашёл два и более ученика")
 
 
